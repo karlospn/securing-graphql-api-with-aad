0c3780b70fbfae6ee7a1d3c4ebc044ce48544c0d
Carlos Pons <karlospn@gmail.com>
2021-08-15 19:02:30 +0200

    Scaffold new sln

---
+++ src/GraphQL.Client/Controllers/InvokeGraphApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Client.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/invoke")]
+    public class InvokeGraphApiController : ControllerBase
+    {
+
+        private readonly ILogger<InvokeGraphApiController> _logger;
+
+        public InvokeGraphApiController(ILogger<InvokeGraphApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
